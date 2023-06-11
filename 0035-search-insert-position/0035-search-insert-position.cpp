695489eabc8ffbc71297d6469dcea7c2ae505db7
Parteek <72187356+parteekahlawat@users.noreply.github.com>
2023-06-11 22:56:19 +0530

    Time: 5 ms (62.04%), Space: 9.7 MB (60.43%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-    int bnry_srch(vector<int> ar, int k){
-        int n = ar.size();
-        if(n==0){
-            return -1;
-        }
-        int l = 0, h = n - 1;
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, h = nums.size()- 1;
         int mid;
         while(l<=h){
             mid = (l+h)/2;
-            if(ar[mid]==k){
+            if(nums[mid]==target){
                 return mid;
             }
-            else if(ar[mid]>k){
+            else if(nums[mid]>target){
                 h = mid-1;
             }
-            else if(ar[mid]<k){
+            else if(nums[mid]<target){
                 l = mid+1;
             }
         }
-        if(ar[mid]>k){
-            return mid;
-        }
-        else{
-            return mid+1;
-        }
-    }
-    int searchInsert(vector<int>& nums, int target) {
-        return bnry_srch(nums, target);
+        return l;
     }
 };
