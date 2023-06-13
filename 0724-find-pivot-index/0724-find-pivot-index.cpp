d04e5cbcfebc588b48025d232fa224e0fd54623e
Parteek <72187356+parteekahlawat@users.noreply.github.com>
2023-06-13 17:12:30 +0530

    Time: 15 ms (98.97%), Space: 31.2 MB (50.61%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int ans=-1;
+        int sm1=0, sm2=0;
+        for(int i=1;i<nums.size();i++){
+            sm2+=nums[i];
+        }
+        if(sm1==sm2){
+            return 0;
+        }
+        for(int i=1;i<nums.size();i++){
+
+            sm1+=nums[i-1];
+            sm2-=nums[i];
+            if(sm1==sm2){
+                return i;
+            }
+        }
+        return ans;
+    }
+};
