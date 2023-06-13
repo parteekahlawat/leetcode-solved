437379bdbcc669c9e2d59b45d3363f75cfabda3b
Parteek <72187356+parteekahlawat@users.noreply.github.com>
2023-06-13 18:58:23 +0530

    Time: 34 ms (12.07%), Space: 18 MB (15.80%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
-    int bnry_srch(vector<int> ar,int k){
-        int n = ar.size();
-        int l = 0, h = n - 1;
-        int mid;
-        while(l<=h){
-            mid = (l+h)/2;
-            if(ar[mid]==k){
-                return mid;
-            }
-            else if(ar[mid]>k){
-                h = mid-1;
-            }
-            else if(ar[mid]<k){
-                l = mid+1;
-            }
-        }
-        return -1;
-    }
+
     int missingNumber(vector<int>& nums) {
-        // complexity of O(nlog(n))
+        // time complexity of O(n)
         
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
-        for(int i=0;i<=n;i++){
-            if(bnry_srch(nums, i)==-1){
-                return i;
-            }
+        int sm=0;
+        for(int i:nums){
+            sm+=i;
         }
-        return 0;
+        int n = nums.size();
+        int sm_exp = n*(n+1)/2;
+        return sm_exp-sm;
+
     }
 };
