b680ca9fe1b440e3ef7d2a0259606d7363e86d16
Parteek <72187356+parteekahlawat@users.noreply.github.com>
2024-06-03 20:14:11 +0530

    Time: 17 ms (70.29%), Space: 12 MB (11.40%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2486-append-characters-to-string-to-make-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2486-append-characters-to-string-to-make-subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int i=0, j=0;
+        int ans=0, tmp=0;
+        while((i<s.size()) &&  (j<t.size())){
+            if(s[i]==t[j]){
+                tmp++;
+                i++;
+                j++;
+            }
+            else{
+                i++;
+                ans = max(tmp, ans);
+            }
+        }
+        ans = max(tmp, ans);
+        return (t.size()-ans);
+    }
+};
